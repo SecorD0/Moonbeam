f7eedf2b4ba6a011d1c60380c0fa7b1850435efb
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-09-18 17:14:19 +0300

    Shows node info

--- node_info.sh
+++ node_info.sh
@@ -28,7 +28,8 @@ while test $# -gt 0; do
 		echo -e "         (you can send Pull request with new texts to add a language)"
 		echo -e "https://teletype.in/@letskynode/Moonbeam_EN — English-language a node installation guide"
 		echo -e "https://teletype.in/@letskynode/Moonbeam_RU — Russian-language a node installation guide"
-		echo -e "https://t.me/letskynode — node Community"
+		echo -e "https://t.me/OnePackage — noderun and tech community"
+		echo -e "https://learning.1package.io — guides and articles"
 		echo -e "https://teletype.in/@letskynode — guides and articles"
 		echo
 		return 0 2>/dev/null; exit 0
